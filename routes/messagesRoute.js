d03480739a08229b18e106fcbd15838636530a8c
Yash Chhatre <yashchhatre@Yashs-MacBook-Air.local>
2021-05-02 09:59:45 -0700

    i think i finally fixed heroku

--- routes/messagesRoute.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-const express_1 = __importDefault(require("express"));
-const message_controller_1 = __importDefault(require("../controllers/message.controller"));
-const messageRouter = express_1.default.Router();
-messageRouter.post("/", message_controller_1.default.createMessage);
-module.exports = messageRouter;
