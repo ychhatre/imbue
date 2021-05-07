04a92145e3cc2a0d630b45382b4d607e3c357de7
Yash Chhatre <yashchhatre@Yashs-MacBook-Air.local>
2021-05-06 22:50:04 -0700

    removed bycrpt

--- src/components/Auth/signIn.js
+++ src/components/Auth/signIn.js
@@ -12,7 +12,6 @@ import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import { useHistory } from "react-router-dom";
 import { useAuth } from "../../contexts/AuthContext";
-import bcrypt from "bcryptjs"; 
 const useStyles = makeStyles((theme) => ({
   paper: {
     marginTop: theme.spacing(8),
