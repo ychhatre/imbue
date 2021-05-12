5609155a609b34c9a953c8b60e069f7d8c5d15e9
Yash Chhatre <yashchhatre@Yashs-MacBook-Air.local>
2021-05-12 15:44:17 -0700

    removed create.js

--- src/App.js
+++ src/App.js
@@ -20,7 +20,6 @@ function App() {
           <PrivateRoute exact path="/" component={Home} />
           <PrivateRoute path="/settings" component={Settings} />
           <PrivateRoute path = "/search/:id" component={Search} />
-          {/* <PrivateRoute path="/profile" component={Profile} /> */}
         </Switch>
       </AuthProvider>
     </Router>
